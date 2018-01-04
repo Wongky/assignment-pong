2649ab0665a2cf08e336bafd6698d7fac6b07fa7
Wongky <32792743+Wongky@users.noreply.github.com>
2018-01-04 20:10:18 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-git init
-git remote add origin https://github.com/hkust-smartcar/assignment-pong
-git pull origin master
-git remote rm origin
-git remote add origin https://github.com/Wongky/assignment-pong.git
-git add *
-git commit -a -m "initial commit"
-git push --set-upstream origin master
