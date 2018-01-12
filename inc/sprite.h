7c3c49f4676120355738d1207fed95c0b52211d8
ytlamal <ytlamal@connect.ust.hk>
2018-01-12 16:09:05 +0800

    Update sprite.h

--- inc/sprite.h
+++ inc/sprite.h
@@ -44,31 +44,48 @@ public:
      * Constructor of sprite which accepts a sprite config.
      * @param config Config object to initialize default values.
      */
-    explicit Sprite(Config config);
+    explicit Sprite(Config config):m_width(config.width),m_height(config.height),m_fg_color(config.fg_color),m_bg_color(config.bg_color){
+    	//m_width=config.width;
+		//m_height=config.height;
+		//m_fg_color=config.fg_color;
+		//m_bg_color=config.bg_color;
+		m_position.x=config.x;
+		m_position.y=config.y;
+		m_pLcd=config.pLcd;
+    };
 
     /**
      * Renders the sprite
      */
-    virtual void render() = 0;
+    virtual void render(){
+    	draw();
+    }
 
     /**
      * Sets position of sprite using 2 integers x and y.
      * @param x Position x.
      * @param y Position y.
      */
-    void setPosition(int x, int y);
+    void setPosition(int x, int y){
+    	m_position.x=x;
+    	m_position.y=y;
+    }
 
     /**
      * Sets position of sprite using a Coord.
      * @param coord Coordinate containing position x and y.
      */
-    void setPosition(Coord coord);
+    void setPosition(Coord coord){
+    	m_position = coord;
+    };
 
     /**
      * Gets position of the sprite.
      * @return position of sprite with Coord.
      */
-    Coord getPosition();
+    Coord getPosition(){
+    	return m_position;
+    }
 
 protected:
     /**
@@ -106,12 +123,19 @@ protected:
     /**
      * Draws the sprite in the new frame.
      */
-    virtual void draw();
+    virtual void draw(){
+   	 clean();
+   	 libsc::Lcd::Rect nrect{m_position.x,m_position.y,m_width,m_height};
+   	 m_pLcd->SetRegion(nrect);
+   	 m_pLcd->FillColor(0x001F);
+    }
 
     /**
      * Removes the sprite with the previous location in the new frame.
      */
-    virtual void clean();
+    virtual void clean(){
+    	 m_pLcd->ClearRegion();
+    }
 };
 
 
