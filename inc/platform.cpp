fc676a792fd9e89f39f46b894e16256ce201900c
ytlamal <ytlamal@connect.ust.hk>
2018-01-14 08:15:08 +0800

    Update platform.cpp

--- inc/platform.cpp
+++ inc/platform.cpp
@@ -1,6 +1,6 @@
 /* platform.cpp
  *
- *  Created on: 2018年1月6日
+ *  Created on: 2018
  *      Author: User
  */
 
@@ -14,6 +14,7 @@
 	m_height=4;
 	m_position.x=60;
 	m_position.y=8;
+	m_fg_color=  0x0000;
 
 
 
@@ -41,6 +42,45 @@
      * Renders the platform.
      */
     void Platform::render(){
+    	clean();
+    	draw();
+    };
+	Platformsl::Platformsl(libsc::Lcd* pLcd){
+
+
+	m_pLcd=pLcd;
+	m_width=30;
+	m_height=4;
+	m_position.x=60;
+	m_position.y=142;
+	m_fg_color=  0x0000;
+
+
+
+}
+/**
+     * Moves the platform one step to the left.
+     */
+    void Platformsl::moveRight(){
+    	if(m_position-4>=4){
+    	m_position -=4;
+    	}
+    };
+
+    /**
+     * Moves the platform one step to the right.
+     */
+    void Platformsl::moveLeft(){
+
+    	if(m_position+4>=116){
+    	    	m_position +=4;
+    	    	}
+    };
+
+    /**
+     * Renders the platform.
+     */
+    void Platformsl::render(){
     	clean();
     	draw();
     };
