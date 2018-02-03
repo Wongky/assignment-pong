d7642df5895aa7a446997e407e922557250633fc
ytlamal <ytlamal@connect.ust.hk>
2018-02-03 17:31:40 +0800

    Update game_config.h

--- inc/game_config.h
+++ inc/game_config.h
@@ -1,7 +1,7 @@
 /*
  * game_config.h
  *
- *  Created on: 2018
+ *  Created on: 2018年1月13日
  *      Author: User
  */
 
@@ -97,6 +97,88 @@ public:
 };
 
 
+class Judge4:public Sprite {
+ public:
+	explicit Judge4(libsc::Lcd* pLcd):
+	Sprite([this](libsc::Lcd* plcd){
+		Sprite::Config config;
+		config.pLcd=plcd;
+		config.width=6;
+		config.height=4;
+		config.x=116;
+		config.y=146;
+		config.fg_color=  0x07E0;
+		return config;
+	 }(pLcd)) {};
+
+
+    //Ball *ball = nullptr;
+
+	bool mark=false;
+	void addmark(){
+		mark=true;
+		draw();
+	}
+	void render(){
+		if(mark==true){
+			draw();
+		}
+	}
+};
+
+class Judge5:public  Sprite {
+public:
+	explicit Judge5(libsc::Lcd* pLcd):Sprite([this](libsc::Lcd* plcd){
+			Sprite::Config config;
+			config.pLcd=plcd;
+			config.width=6;
+			config.height=4;
+			config.fg_color=  0x07E0;
+			config.x=106;
+			config.y=146;
+			return config;
+		 }(pLcd)) {};
+
+    //Ball *ball = nullptr;
+	bool mark=false;
+	void addmark(){
+		mark=true;
+		draw();
+		}
+	void render(){
+			if(mark==true){
+				draw();
+			}
+		}
+};
+
+
+class Judge6: public Sprite {
+public:
+	explicit Judge6(libsc::Lcd* pLcd):Sprite([this](libsc::Lcd* plcd){
+			Sprite::Config config;
+			config.pLcd=plcd;
+			config.width=6;
+			config.height=4;
+			config.fg_color=  0x07E0;
+			config.x=96;
+			config.y=146;
+			return config;
+		 }(pLcd)) {};
+
+    //Ball *ball = nullptr;
+	bool mark=false;
+	void addmark(){
+		mark=true;
+		draw();
+		}
+	void render(){
+			if(mark==true){
+				draw();
+			}
+		}
+};
+
 
 class FinalJudge: public Sprite {
 public:
@@ -131,6 +213,41 @@ public:
 	    }
 	};
 };
+class FinalJudgesl: public Sprite {
+public:
+	explicit FinalJudgesl(libsc::Lcd* pLcd):Sprite([this](libsc::Lcd* plcd){
+			Sprite::Config config;
+			config.pLcd=plcd;
+			config.width=6;
+			config.height=4;
+			config.fg_color=  0x07E0;
+			config.x=96;
+			config.y=0;
+			return config;
+		 }(pLcd)) {};
+	void setjudge(Judge4 * j1,Judge5 * j2,Judge6 * j3){
+		mj1=j1;
+		mj2=j2;
+		mj3=j3;
+	}
+    Judge4*mj1 =nullptr;
+    Judge5*mj2 =nullptr;
+    Judge6*mj3 =nullptr;
+	//Ball*ball = nullptr;
+    //Ball* b = nullptr;
+	bool win=false;
+	void countmark(){
+		if(mj1->mark==false&&mj2->mark==false&&mj3->mark==false){mj1->addmark();}
+		else if(mj1->mark==true&&mj2->mark==false&&mj3->mark==false){mj2->addmark();}
+		else if(mj1->mark==true&&mj2->mark==true&&mj3->mark==false){mj3->addmark();}
+		else if(mj1->mark==true&&mj2->mark==true&&mj3->mark==true){
+			win=true;
+			//LcdTypewriter writer(Config::GetWriterConfig(&m_pLcd));
+	    }
+	};
+};
+
+
 
 
 #endif /* INC_GAME_CONFIG_H_ */
